7de86114fef754b99b8651e502650b8e60b91e0b
Monika-Mistry <monika.mistry1@gmail.com>
2019-08-05 16:24:05 +0100

    restcontroller

---
+++ src/main/java/com/bae/rest/AccountController.java
@@ -0,0 +1,54 @@
+package com.bae.rest;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.bae.domain.Account;
+import com.bae.service.AccountService;
+
+@RestController
+public class AccountController {
+
+	private AccountService accountService;
+
+	@Autowired
+	public AccountController(AccountService accountService) {
+		this.accountService = accountService;
+	}
+
+	public AccountController() {
+
+	}
+
+	@GetMapping
+	public List<Account> findAll() {
+		return accountService.findAll();
+
+	}
+	
+	@PostMapping
+	public ResponseEntity<Account> createAccount(@RequestBody Account account) {
+		Account newAccount = accountService.createAccount(account);
+		return new ResponseEntity<>(newAccount, HttpStatus.CREATED);
+	}
+	
+	@DeleteMapping
+	public String deleteAccount(Account account) {
+		return accountService.deleteAccount(account);
+	}
+	
+	@PutMapping
+	public String updateAccount(Account account) {
+		return accountService.updateAccount(account);
+	}
+
+}
