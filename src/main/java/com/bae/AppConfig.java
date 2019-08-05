41d9f3cb6990982bbbe453d329bcef397dacc5fd
Monika-Mistry <monika.mistry1@gmail.com>
2019-08-05 16:30:42 +0100

    @Service annotation

---
+++ src/main/java/com/bae/AppConfig.java
@@ -0,0 +1,7 @@
+package com.bae;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AppConfig {
+
+}
--- src/main/java/com/bae/service/AccountServiceImpl.java
+++ src/main/java/com/bae/service/AccountServiceImpl.java
@@ -3,10 +3,12 @@ package com.bae.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.bae.domain.Account;
 import com.bae.repository.AccountRepository;
 
+@Service
 public class AccountServiceImpl implements AccountService {
 	
 	private AccountRepository accountRepository;
