0a51cc047d87ec50a8c772155a1cf2207da9ae28
Monika-Mistry <monika.mistry1@gmail.com>
2019-08-06 15:16:02 +0100

    controller test written

---
+++ AccountNumberGeneratorAPI/src/main/java/com/bae/rest/NumberController.java
@@ -0,0 +1,10 @@
+package com.bae.rest;
+
+public class NumberController {
+
+	public Object getAccountNumber() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ AccountNumberGeneratorAPI/src/test/java/com/bae/rest/NumberControllerTest.java
@@ -0,0 +1,34 @@
+package com.bae.rest;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.when;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import com.bae.business.NumberGenerator;
+
+@RunWith(SpringRunner.class)
+public class NumberControllerTest {
+	
+	@InjectMocks
+	private NumberController controller;
+	
+	@Mock
+	private NumberGenerator numgen;
+	
+	private static final String MOCK_ACCOUNT_NUMBER = "A123456";
+	
+	@Test
+	public void getAccountNumberTest() {
+		when(numgen.getAccountNumber()).thenReturn(MOCK_ACCOUNT_NUMBER);
+		
+		assertEquals(MOCK_ACCOUNT_NUMBER, controller.getAccountNumber());
+	}
+	
+	
+
+}
