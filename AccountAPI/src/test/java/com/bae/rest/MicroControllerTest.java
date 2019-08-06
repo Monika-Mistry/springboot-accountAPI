a99af6f8bb24fe738723db3c73a071779973f230
Monika-Mistry <monika.mistry1@gmail.com>
2019-08-06 17:01:53 +0100

    mciro test

--- AccountAPI/src/test/java/com/bae/rest/MicroControllerTest.java
+++ AccountAPI/src/test/java/com/bae/rest/MicroControllerTest.java
@@ -1,5 +1,6 @@
 package com.bae.rest;
 
+import static org.junit.Assert.assertEquals;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -12,23 +13,28 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.web.client.RestTemplate;
 
 @RunWith(SpringRunner.class)
 @WebMvcTest(MicroController.class)
 @AutoConfigureMockMvc
 public class MicroControllerTest {
-	
+
 	@Autowired
 	private MockMvc mockMvc;
-	
+
 	@MockBean
 	private RestTemplate restTemplate;
-	
+
 	@Test
 	public void getAccountNumberTest() throws Exception {
+
+		MvcResult result = mockMvc.perform(get("/getAccNo")).andDo(print()).andReturn();
 		
-		mockMvc.perform(get("/getAccNo")).andExpect(status().isOk()).andDo(print());
+		System.out.println(result.getResponse().getContentAsString());
+		assertEquals(true, result.getResponse().getContentAsString().matches("[ABC]\\d{6}"));
+
 	}
 
 }
--- AccountNumberGeneratorAPI/src/main/java/com/bae/rest/NumberController.java
+++ AccountNumberGeneratorAPI/src/main/java/com/bae/rest/NumberController.java
@@ -1,9 +1,7 @@
 package com.bae.rest;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.bae.business.NumberGenerator;
@@ -22,11 +20,9 @@ public class NumberController {
 		this.numberGenerator = numberGenerator;
 	}
 
-	@RequestMapping("/getAccNo")
-	public ResponseEntity<String> getAccountNumber() {
-		String accNo =  numberGenerator.getAccountNumber();
-		
-		return new ResponseEntity<String>(accNo, HttpStatus.OK);
+	@GetMapping("/getAccNo")
+	public String getAccountNumber() {		
+		return numberGenerator.getAccountNumber();
 	}
 
 }
