e2ec5e9b16364f38ebcb1c224f425ce26616b88d
Monika-Mistry <monika.mistry1@gmail.com>
2019-08-07 10:15:54 +0100

    testing added

--- AccountAPI/src/main/java/com/bae/rest/MicroController.java
+++ AccountAPI/src/main/java/com/bae/rest/MicroController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
@@ -13,6 +14,7 @@ public class MicroController {
 	private RestTemplate restTemplate;
 
 	private static final String ACC_NO_URL = "http://localhost:8081/getAccNo";
+	private static final String PRIZE_URL = "http://localhost:8082/getPrize/";
 
 	@Autowired
 	public MicroController(RestTemplate restTemplate) {
@@ -26,6 +28,11 @@ public class MicroController {
 	@GetMapping("/getAccNo")
 	public ResponseEntity<String> getAccountNumber() {
 		return restTemplate.exchange(ACC_NO_URL, HttpMethod.GET, null, String.class);
+	}
+	
+	@GetMapping("/getPrize/{accountNo}")
+	public ResponseEntity<String> getPrize(@PathVariable("accountNo") String accountNumber) {
+		return restTemplate.exchange(PRIZE_URL + accountNumber, HttpMethod.GET, null, String.class);
 	}
 
 }
